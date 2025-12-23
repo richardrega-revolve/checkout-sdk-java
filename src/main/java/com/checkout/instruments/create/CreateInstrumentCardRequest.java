8c4eeeed2f85577a5aeafd1c12bff0b920cca8fb
richardrega-revolve <59029288+richardrega-revolve@users.noreply.github.com>
2025-12-23 11:54:26 -0800

    Create CreateInstrumentCardRequest.java
    Java file

---
+++ src/main/java/com/checkout/instruments/create/CreateInstrumentCardRequest.java
@@ -0,0 +1,35 @@
+package com.checkout.instruments.create;
+
+import com.checkout.common.AccountHolder;
+import com.checkout.common.InstrumentType;
+import com.google.gson.annotations.SerializedName;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public final class CreateInstrumenCardRequest extends CreateInstrumentRequest {
+
+    @SerializedName("account_holder")
+    private AccountHolder accountHolder;
+
+    private CreateCustomerInstrumentRequest customer;
+
+    @Builder
+    private CreateInstrumenCardRequest(final AccountHolder accountHolder,
+                                         final CreateCustomerInstrumentRequest customer) {
+        super(InstrumentType.CARD);
+        this.accountHolder = accountHolder;
+        this.customer = customer;
+    }
+
+    public CreateInstrumenCardRequest() {
+        super(InstrumentType.CARD);
+    }
+
+}
